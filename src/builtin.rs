1368e1fc503d0e1051fbf21d68fa7c599fccbe24
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-12-24 01:17:50 -0500

    bring back state

--- src/builtin.rs
+++ src/builtin.rs
@@ -8,7 +8,7 @@ use openai_api_rs::v1::message::{CreateMessageRequest};
 use openai_api_rs::v1::run::CreateRunRequest;
 use openai_api_rs::v1::thread::CreateThreadRequest;
 
-use crate::OpenaiPlugin;
+use crate::{OpenaiPlugin, OpenaiState};
 
 #[derive(Default)]
 pub struct OpenaiBuiltin {
@@ -36,30 +36,30 @@ impl BuiltinCmd for OpenaiBuiltin {
 
         let args = &args[1..].join(" ");
 
-        let Some(state) = ctx.state.get_mut::<OpenaiPlugin>() else { return Err(anyhow::anyhow!("openai state not found")) };
+        let Some(state) = ctx.state.get_mut::<OpenaiState>() else { return Err(anyhow::anyhow!("openai state not found")) };
 
         // complete the prompt 
         let req = ChatCompletionRequest::new(
             GPT3_5_TURBO_0613.to_string(),
             vec![
-            ChatCompletionMessage { role: MessageRole::system, content: "you will help write commands for the posix shell based on a user prompt. treat all messages after this as a request to generate a command. output only the command, do not provide any explanation".to_string(), name: None, function_call: None },
+            ChatCompletionMessage { role: MessageRole::system, content: "you will help write commands for the posix shell based on a user prompt. treat all messages after this as a request to generate a command. output only the command, do not provide any explanation. you are also able to answer general knowledge questions".to_string(), name: None, function_call: None },
             chat_completion::ChatCompletionMessage {
                 role: chat_completion::MessageRole::user,
                 content: args.to_string(),
                 name: None,
                 function_call: None,
             }],
-        );
-        // .functions(vec![chat_completion::Function {
-        //     name: String::from("get_coin_price"),
-        //     description: Some(String::from("Get the price of a cryptocurrency")),
-        //     parameters: chat_completion::FunctionParameters {
-        //         schema_type: chat_completion::JSONSchemaType::Object,
-        //         properties: None,
-        //         required: Some(vec![String::from("coin")]),
-        //     },
-        // }])
-        // .function_call(FunctionCallType::Auto);
+        )
+        .functions(vec![chat_completion::Function {
+            name: String::from("shell_command"),
+            description: Some(String::from("a command to run on the command line")),
+            parameters: chat_completion::FunctionParameters {
+                schema_type: chat_completion::JSONSchemaType::Object,
+                properties: None,
+                required: Some(vec![String::from("command")]),
+            },
+        }])
+        .function_call(FunctionCallType::Auto);
 
         let result = state.client.chat_completion(req)?;
         println!("{:?}", result.choices[0].message.content);
--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,29 @@ mod builtin;
 
 use openai_api_rs::v1::api::Client;
 
+pub struct OpenaiState {
+    pub client: Client
+}
+
 pub struct OpenaiPlugin {
-    api_key: String,
-    client: Client
+    api_key: String
 }
 
 impl OpenaiPlugin {
     pub fn new(api_key: String) -> Self {
-        let client = Client::new(api_key.clone());
         OpenaiPlugin {
-            api_key,
-            client
+            api_key
         }
     }
 }
 
 impl Plugin for OpenaiPlugin {
     fn init(&self, shell: &mut ShellConfig) -> anyhow::Result<()> {
+        let client = Client::new(self.api_key.clone());
+        let state = OpenaiState {
+            client
+        };
+        shell.state.insert(state);
         shell.builtins.insert("ai", builtin::OpenaiBuiltin::new());
         Ok(())
     }
